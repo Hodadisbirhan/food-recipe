20ba4155a464c17967e4e5c0f13117b0e4773440
hod <hod@gmail.com>
2022-05-22 00:59:36 +0300

    last commit

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,6 +1,6 @@
 <template>
  <div class="w-full h-full justify-center items-center flex relative" >
-   <img src="../assets/pagenotfound.jpg" class=" absolute top-1 bottom-1 right-0 left-0" alt="notfound"/>
+   <span class="text-center w-full text-black text-3xl font-bold">404 PAGE NOT FOUND</span>
    <button class="w-36 h-8  bg-slate-900 text-white rounded-md text-center drop-shadow-xl" @click="home">Back To HOme</button>
 
 
