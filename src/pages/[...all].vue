fa206ad09890c363401b791ee8e8a157002b8d0d
hod <hod@gmail.com>
2022-05-21 22:24:29 +0300

    last commit

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,6 +1,6 @@
 <template>
  <div class="w-full h-full justify-center items-center flex relative" >
-   <img src="../assets/pagenotfound.png" class=" absolute top-1 bottom-1 right-0 left-0" alt="notfound"/>
+   <img src="../assets/pagenotfound.jpg" class=" absolute top-1 bottom-1 right-0 left-0" alt="notfound"/>
    <button class="w-16 h-8 bg-slate-900 text-center drop-shadow-xl" @click="home">Back To HOme</button>
 
 
