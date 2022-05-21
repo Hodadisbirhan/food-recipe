4d82b468cb88dc6c88d5eaeb5c2ebf430d6b0f15
hod <hod@gmail.com>
2022-05-21 23:38:35 +0300

    last commit

--- src/components/Ingredient.vue
+++ src/components/Ingredient.vue
@@ -6,6 +6,6 @@ const props = defineProps({
 </script>
 <template>
   <li class="flex gap-1 rounded-md py-3 px-2  bg-white">
-    <p class="text-base break-all text-slate-800">{{ props.name }}</p>
+    <p class="text-base break-all ">{{ props.name }}</p>
   </li>
 </template>
