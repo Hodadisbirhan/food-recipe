0c230cc6c07293be8c867dc028c14d488efc8b73
hod <hod@gmail.com>
2022-05-21 19:22:05 +0300

    last commit

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,3 +1,23 @@
 <template>
-  <h1 class="text-2xl font-bold">ERROR</h1>
+ <div class="w-full h-full justify-center items-center flex relative" >
+   <img src="../assets/pagenotfound.png" class=" absolute top-1 bottom-1 right-0 left-0" alt="notfound"/>
+   <button class="w-16 h-8 bg-slate-900 text-center drop-shadow-xl" @click="home">Back To HOme</button>
+
+
+ </div>
 </template>
+<script>
+import {useRouter} from "vue-router";
+const router = useRouter();
+
+const home=()=>{
+
+
+router.replace('/');
+
+}
+
+
+</script>
+
+
