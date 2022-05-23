b04e8f245ae7f4836e581acd79004b75627942c8
hod <hod@gmail.com>
2022-05-23 21:54:03 +0300

    last commit

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,5 +1,5 @@
 <template>
- <div class="w-full h-full justify-center items-center flex relative" >
+ <div class="w-full h-screen justify-center flex-col gap-3 items-center flex relative" >
    <span class="text-center w-full text-black text-3xl font-bold">404 PAGE NOT FOUND</span>
    <button class="w-36 h-8  bg-slate-900 text-white rounded-md text-center drop-shadow-xl" @click="home">Back To HOme</button>
 
@@ -13,7 +13,7 @@ const router = useRouter();
 const home=()=>{
 
 
-router.replace('/');
+router.replace("/index");
 
 
 }
