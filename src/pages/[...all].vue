ff357e7c08e84388b18fa3b01d28d242286346cf
hod <hod@gmail.com>
2022-05-23 21:56:37 +0300

    last commit

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,7 +1,7 @@
 <template>
  <div class="w-full h-screen justify-center flex-col gap-3 items-center flex relative" >
    <span class="text-center w-full text-black text-3xl font-bold">404 PAGE NOT FOUND</span>
-   <button class="w-36 h-8  bg-slate-900 text-white rounded-md text-center drop-shadow-xl" @click="home">Back To HOme</button>
+   <button class="w-36 h-8  bg-slate-900 text-white rounded-md text-center drop-shadow-xl" @click="home()">Back To HOme</button>
 
 
  </div>
